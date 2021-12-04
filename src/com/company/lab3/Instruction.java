730e45e67929b963cd1545cd03e5e1d22f68d9f8
CaptainShepard01 <49350081+CaptainShepard01@users.noreply.github.com>
2021-12-04 19:35:50 +0200

    GUI fix to show pageNumbers correctly

---
+++ src/com/company/lab3/Instruction.java
@@ -0,0 +1,11 @@
+package com.company.lab3;
+
+public class Instruction {
+    public String inst;
+    public long addr;
+
+    public Instruction(String inst, long addr) {
+        this.inst = inst;
+        this.addr = addr;
+    }
+}
